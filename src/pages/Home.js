d635cc7efadfefc19337c568b6fd86e9dce655a3
Karmhhh <carmelapia01@gmail.com>
2024-01-03 11:40:36 +0100

    home modificata

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,9 @@ import img3 from "../Assets/ArtGalleryimg3.jpg";
 import img4 from "../Assets/Ellipseartist.png";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
 
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 const images = [
   {
     title:
@@ -43,23 +45,55 @@ export const Home = () => {
       </Slide>
       {/* web */}
       <Box sx={{ display: { xs: "none", md: "block" } }}>
+      <Grid container spacing={2} direction={"row"} className="row-img">
+      <Grid item lg={4}>
+           <img src={img1} alt="img" />
+         </Grid>
+         <Grid item container direction={'column'} lg={3} >
+           
+           <Grid item  md={4}> 
+             <span>
+               <Typography component={'i'} variant="h3">For the Clients  </Typography>
+               <Typography component={'i'} variant="body1">Description</Typography>
+             </span>
+           </Grid>
+
+             <Grid item container direction={"row"}>
+               <Grid item md={6}>
+                 <Button component="label" variant="text" style={{color:'white'}} startIcon={<KeyboardDoubleArrowLeftIcon />}> Log In</Button>
+               </Grid> 
+                 <Grid item  md={6}>
+                 <Button component="label"  variant="text" style={{color:'white'}} endIcon={<KeyboardDoubleArrowRightIcon />}>Sign In</Button>
+                 </Grid>  
+             </Grid>
+         </Grid>
+       
+       </Grid>
         <Grid container spacing={2} direction={"row"} className="row-img">
-          <Grid item lg={4}>
-            <img src={img1} alt="img" />
+         
+          <Grid item container direction={'column'} lg={3} >
+            
+            <Grid item  md={4}> 
+              <span>
+                <Typography component={'i'} variant="h3">For Promoters </Typography>
+                <Typography component={'i'} variant="body1">Description</Typography>
+              </span>
+            </Grid>
+
+              <Grid item container direction={"row"}>
+                <Grid item md={6}>
+                  <Button component="label" variant="text" style={{color:'white'}} startIcon={<KeyboardDoubleArrowLeftIcon />}> Log In</Button>
+                </Grid> 
+                  <Grid item  md={6}>
+                  <Button component="label"  variant="text" style={{color:'white'}} endIcon={<KeyboardDoubleArrowRightIcon />}>Sign In</Button>
+                  </Grid>  
+              </Grid>
           </Grid>
-          <Grid item lg={7}>
-            <span>For Artist</span>
-          </Grid>{" "}
-        </Grid>
-        <Grid container spacing={2} direction={"row"} className="row-img">
-          <Grid item lg={7}>
-            <span>For Promoter</span>
-          </Grid>{" "}
           <Grid item lg={4}>
             <img src={img2} alt="img" />
           </Grid>
         </Grid>
-        <Grid container spacing={2} direction={"row"} className="row-img">
+        <Grid container spacing={1} direction={"row"} className="row-img">
           <Grid item lg={4}>
             <img src={img3} alt="img" />
           </Grid>
@@ -83,7 +117,7 @@ export const Home = () => {
           <Grid item xs={4}>
           <span>
               <Typography component={"i"} variant={"h6"}>
-                For Artist
+                For Clients
               </Typography>
             </span>
           </Grid>{" "}
